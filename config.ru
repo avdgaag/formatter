0e10d17f88823d044cf4e7e9933c106919beb15d
Arjan van der Gaag <arjan@Kabisa.nl>
2012-07-29 12:40:43 +0200

    Move Sass load path config to config.ru

--- config.ru
+++ config.ru
@@ -3,4 +3,7 @@ require 'bundler'
 Bundler.require
 require 'formatter'
 Formatter::App.set :root, File.expand_path('../', __FILE__)
+Formatter::App.set :scss, load_paths: [
+  File.join(Bundler.load.specs.find { |s| s.name == 'bourbon' }.full_gem_path, 'app', 'assets', 'stylesheets')
+]
 run Formatter::App
--- lib/formatter/app.rb
+++ lib/formatter/app.rb
@@ -1,9 +1,7 @@
 module Formatter
   class App < Sinatra::Base
     set :haml, format: :html5
-    set :scss, style: :compressed, load_paths: [
-      File.join(Bundler.load.specs.find { |s| s.name == 'bourbon' }.full_gem_path, 'app', 'assets', 'stylesheets')
-    ]
+    set :scss, style: :compressed
 
     get '/styles.css' do
       headers 'Content-Type' => 'text/css; charset=utf-8'
